6a985634afb8deb1bedb2b199ba2b2826918d521
Prasad Magar <prasadmagar0938@gmail.com>
2025-12-25 23:14:50 +0530

    Update the SecurityConfig.java

--- E-CommerceApp/src/main/java/com/example/main/config/SecurityConfig.java
+++ E-CommerceApp/src/main/java/com/example/main/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder() { 
         return new BCryptPasswordEncoder();
     }
 }
