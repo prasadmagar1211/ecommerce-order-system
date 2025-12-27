12b57485f6137cdc9e8cc6d736fb24643a484e9a
Prasad Magar <prasadmagar0938@gmail.com>
2025-12-27 18:25:24 +0530

    Change the space in CustomUserDetails.java

--- E-CommerceApp/src/main/java/com/example/main/services/CustomUserDetailsService.java
+++ E-CommerceApp/src/main/java/com/example/main/services/CustomUserDetailsService.java
@@ -21,5 +21,5 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
 				new ArrayList<>());
-	}
+	} 
 }
